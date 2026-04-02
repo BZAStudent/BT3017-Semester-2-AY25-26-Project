ea479c42aa2191bddc7d3654595b3eda73e2cb08
BZAStudent <e1138748@u.nus.edu>
2026-04-02 15:32:59 +0800

    Updated Home.py

--- Home.py
+++ Home.py
@@ -16,8 +16,9 @@ st.markdown(
 
 st.markdown('<div class="main-box">', unsafe_allow_html=True)
 st.title("Graph Laplacian Visualiser")
+st.subheader("BT3017 Project")
 st.subheader("Created by Tay Jing Yao")
-st.write("Visualise graph structures, spectral clustering, and influence propagation.")
+st.write("Visualise graph structures, spectral clustering, and range of influence propagation.")
 
 if st.button("Start", use_container_width=False):
     st.switch_page("pages/1_GraphSelection.py")
